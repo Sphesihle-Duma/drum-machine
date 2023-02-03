6e638e5570e7291533e80298f518d55f036562ba
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-03 23:09:26 +0200

    adding mark-down project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const data = [{
+  keyCode: 81,
+  keyTrigger: 'Q',
+  id: 'Heater-1',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+},
+{
+  keyCode: 87,
+  keyTrigger: 'W',
+  id: 'Heater-2',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+},
+{
+  keyCode: 69,
+  keyTrigger: 'E',
+  id: 'Heater-3',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+},
+{
+  keyCode: 65,
+  keyTrigger: 'A',
+  id: 'Heater-4',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+},
+{
+  keyCode: 83,
+  keyTrigger: 'S',
+  id: 'Clap',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+},
+{
+  keyCode: 68,
+  keyTrigger: 'D',
+  id: 'Open-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+},
+{
+  keyCode: 90,
+  keyTrigger: 'Z',
+  id: "Kick-n'-Hat",
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+},
+{
+  keyCode: 88,
+  keyTrigger: 'X',
+  id: 'Kick',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+},
+{
+  keyCode: 67,
+  keyTrigger: 'C',
+  id: 'Closed-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+}
+]
+
+
 
 function App() {
+
+  const[description,setDescription]=useState(null)
+  useEffect(()=>{
+    document.addEventListener("keydown",(event)=>{
+     
+      handleClick(event.key);
+    })
+  },[])
+
+
+  const handleClick = (id) => {
+    console.log(id);
+    
+    const sound =document.getElementById(id);
+    setDescription(id);
+    sound.play();
+  }
+
+  const drum_pad = data.map(obj => <div className="drum-pad" key={obj.id} onClick={() => handleClick(obj.keyTrigger)} id={obj.id}><h1>{obj.keyTrigger}</h1><audio className="clip" id={obj.keyTrigger} src={obj.url} /></div>)
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine">
+      <div id="display">
+      
+      {drum_pad}
+      {description && <h1>{description}</h1>}
+      </div>
+      
     </div>
   );
 }
